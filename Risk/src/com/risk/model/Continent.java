2b75b02a0b718a627cce9462c61d7b7a476ed370
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-07 18:50:29 -0400

    Added constructor, getter and setter methods

--- Risk/src/com/risk/model/Continent.java
+++ Risk/src/com/risk/model/Continent.java
@@ -1,7 +1,9 @@
 package com.risk.model;
 
+import java.util.ArrayList;
+
 /**
- * Model class for Continet
+ * Model class for Continent
  * @author Hareesh Kavumkulath
  * @version 1.0
  * 
@@ -11,5 +13,44 @@ package com.risk.model;
  *
  */
 public class Continent {
+	
+	String name;
+	int numberOfArmies;
+	ArrayList<Continent> adjacentContinents = new ArrayList<Continent>();
+	
+	/**
+	 * Construct a continent with the name and number of Armies
+	 * 
+	 * @param name
+	 * @param numberOfArmies
+	 */
+	public Continent(String name, int numberOfArmies) {
+		this.name = name;
+		this.numberOfArmies = numberOfArmies;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumberOfArmies() {
+		return numberOfArmies;
+	}
+
+	public void setNumberOfArmies(int numberOfArmies) {
+		this.numberOfArmies = numberOfArmies;
+	}
+
+	public ArrayList<Continent> getAdjacentContinents() {
+		return adjacentContinents;
+	}
+
+	public void setAdjacentContinents(ArrayList<Continent> adjacentContinents) {
+		this.adjacentContinents = adjacentContinents;
+	}	
 
 }
