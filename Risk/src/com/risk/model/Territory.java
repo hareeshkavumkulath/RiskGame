6e5f8cbe2ccdbd7681d0fe051a3746fce7c786a6
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-07 18:57:13 -0400

    Added constructors, getter and setters to Territory

--- Risk/src/com/risk/model/Territory.java
+++ Risk/src/com/risk/model/Territory.java
@@ -3,6 +3,8 @@
  */
 package com.risk.model;
 
+import java.util.ArrayList;
+
 /**
  * Model class for the Territory
  * 
@@ -16,6 +18,42 @@ package com.risk.model;
  */
 public class Territory {
 	
+	String name;
+	String continent;
+	ArrayList<String> adjacentTerritories;
+	/**
+	 * Create a Territory with territory name and continent name
+	 * @param name - Name of the Territory
+	 * @param continent - Name of the Continent
+	 */
+	public Territory(String name, String continent) {
+		super();
+		this.name = name;
+		this.continent = continent;
+	}
 	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getContinent() {
+		return continent;
+	}
+	
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+	
+	public ArrayList<String> getAdjacentTerritories() {
+		return adjacentTerritories;
+	}
+	
+	public void setAdjacentTerritories(ArrayList<String> adjacentTerritories) {
+		this.adjacentTerritories = adjacentTerritories;
+	}	
 
 }
