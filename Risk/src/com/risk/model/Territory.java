e24ffeb107a956331692d698b7e173b2bca63814
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-07 18:54:14 -0400

    Added model class Territory

---
+++ Risk/src/com/risk/model/Territory.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.risk.model;
+
+/**
+ * Model class for the Territory
+ * 
+ * @author Hareesh Kavumkulath
+ * @version 1.0
+ * 
+ * Created Date: 10-07-2019
+ * Last Modified Date: 
+ * Last Modified By:
+ *
+ */
+public class Territory {
+	
+	
+
+}
