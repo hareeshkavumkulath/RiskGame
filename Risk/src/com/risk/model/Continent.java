fb97957f2ef5233aa1269e8c943bca117eb2a4da
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-07 18:46:21 -0400

    Added model class for the continents

---
+++ Risk/src/com/risk/model/Continent.java
@@ -0,0 +1,15 @@
+package com.risk.model;
+
+/**
+ * Model class for Continet
+ * @author Hareesh Kavumkulath
+ * @version 1.0
+ * 
+ * Created Date: 10-07-2018
+ * Modified Date:
+ * Modified By:
+ *
+ */
+public class Continent {
+
+}
