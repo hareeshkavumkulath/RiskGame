237ebcea7563c6e8812384b369691e08bc007f7e
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-08 12:31:10 -0400

    Added fields in MapMessage class

--- Risk/src/com/risk/model/MapMessage.java
+++ Risk/src/com/risk/model/MapMessage.java
@@ -3,6 +3,8 @@
  */
 package com.risk.model;
 
+import java.util.ArrayList;
+
 /**
  * Return type of Map Validation method contains ArrayList of Continents,
  * ArrayList of countries, error messages if any and validation status.
@@ -17,6 +19,9 @@ package com.risk.model;
  */
 public class MapMessage {
 	
-	
+	public ArrayList<Territory> countries = new ArrayList<Territory>();
+	public ArrayList<Continent> continents = new ArrayList<Continent>();
+	public boolean isValidMap;
+	public StringBuffer message;
 
 }
