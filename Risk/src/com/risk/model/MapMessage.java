27802cec270385b91434ac6364d9bcccf09921e2
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-08 12:32:47 -0400

    Added constructor, getter and setter methods in MapMessage class

--- Risk/src/com/risk/model/MapMessage.java
+++ Risk/src/com/risk/model/MapMessage.java
@@ -19,9 +19,43 @@ import java.util.ArrayList;
  */
 public class MapMessage {
 	
-	public ArrayList<Territory> countries = new ArrayList<Territory>();
+	public ArrayList<Territory> territories = new ArrayList<Territory>();
 	public ArrayList<Continent> continents = new ArrayList<Continent>();
 	public boolean isValidMap;
 	public StringBuffer message;
+	
+	public MapMessage(ArrayList<Territory> territories, ArrayList<Continent> continents, boolean isValidMap,
+			StringBuffer message) {
+		super();
+		this.territories = territories;
+		this.continents = continents;
+		this.isValidMap = isValidMap;
+		this.message = message;
+	}
+	
+	public ArrayList<Territory> getTerritories() {
+		return territories;
+	}
+	public void setCountries(ArrayList<Territory> territories) {
+		this.territories = territories;
+	}
+	public ArrayList<Continent> getContinents() {
+		return continents;
+	}
+	public void setContinents(ArrayList<Continent> continents) {
+		this.continents = continents;
+	}
+	public boolean isValidMap() {
+		return isValidMap;
+	}
+	public void setValidMap(boolean isValidMap) {
+		this.isValidMap = isValidMap;
+	}
+	public StringBuffer getMessage() {
+		return message;
+	}
+	public void setMessage(StringBuffer message) {
+		this.message = message;
+	}	
 
 }
