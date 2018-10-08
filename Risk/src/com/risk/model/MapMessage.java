fc0179b2df15b8c8359678b5a3a300cfb30d27f6
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-08 12:30:00 -0400

    Added MapMessage.class

---
+++ Risk/src/com/risk/model/MapMessage.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.risk.model;
+
+/**
+ * Return type of Map Validation method contains ArrayList of Continents,
+ * ArrayList of countries, error messages if any and validation status.
+ * 
+ * @author Hareesh Kavumkulath
+ * @version 1.0
+ * 
+ * @date 10-08-2018
+ * @modifiedDate 
+ * @modifiedBy
+ *
+ */
+public class MapMessage {
+	
+	
+
+}
