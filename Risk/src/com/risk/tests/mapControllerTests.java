95850ecd9c421e849ca94b0ad23311d2646f500a
[Hamid] <[heidarzadeh.hamidreza@gmail.com]>
2018-10-15 13:15:35 -0400

    adding test1 for map1 which should return true

--- Risk/src/com/risk/tests/mapControllerTests.java
+++ Risk/src/com/risk/tests/mapControllerTests.java
@@ -1,7 +1,15 @@
 package com.risk.tests;
 
+import com.risk.controller.MapController;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -11,24 +19,59 @@ import org.junit.jupiter.api.BeforeAll;
  * @author Hamid
  */
 
-public class mapControllerTests{
-    
+public class mapControllerTests {
 
     private static String mapFolder;
     private static String map1;
     private static String map2;
     private static String map3;
-    
+    private static MapController mc;
 
     @BeforeAll
-    //Should get the current path and locate the map folder
+    // Should get the current path and locate the map folder
     static void initAll() {
-        mapFolder = System.getProperty("user.dir")+"/maps/";
+        mapFolder = System.getProperty("user.dir") + "/src/com/risk/tests/maps/";
         map1 = mapFolder + "map1.map";
         map2 = mapFolder + "map2.map";
         map3 = mapFolder + "map3.map";
+        mc = new MapController();
+    }
+
+    @Test
+    @DisplayName ("Process Continent Should Return True for MAP1")
+    void mapStringTest1(){
+    		//System.out.println(map1);
+        assertTrue(mc.processContinents(mapToString(map1)));
     }
     
 
+
+    /**
+     * @param mapFile Map File Address as a String
+     * @return Map Content to Strings
+     * @exception IOException if there is a problem with the Map File Address
+     */
+    private static String mapToString(String mapFile) {
+
+        String mapInString = "";
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(mapFile));
+
+            String line = reader.readLine();
+            mapInString += line;
+
+            while (line != null) {
+                mapInString += line;
+                line = reader.readLine();
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            mapInString = null;
+        }
+
+        return mapInString;
+    }
 
 }
