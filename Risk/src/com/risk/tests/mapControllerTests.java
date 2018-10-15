ba343a8279d6148cb19251fa36244c319afc44a4
[Hamid] <[heidarzadeh.hamidreza@gmail.com]>
2018-10-15 12:28:15 -0400

    adding map variables to the class

--- Risk/src/com/risk/tests/mapControllerTests.java
+++ Risk/src/com/risk/tests/mapControllerTests.java
@@ -5,8 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ * @author Hamid
+ */
 
 public class mapControllerTests{
     
+
+    private static String mapFolder;
+    private static String map1;
+    private static String map2;
+    private static String map3;
+    
+    
+
 
 }
