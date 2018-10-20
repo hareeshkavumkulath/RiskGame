a5a357b2b449850a8f1bf41e57b5339228851803
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-10-20 13:35:37 -0400

    imprting required headers for test

--- Risk/src/com/risk/tests/view/FortifyWindowTest.java
+++ Risk/src/com/risk/tests/view/FortifyWindowTest.java
@@ -1,5 +1,10 @@
 package com.risk.tests.view;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FortifyWindowTest{
 
 }
