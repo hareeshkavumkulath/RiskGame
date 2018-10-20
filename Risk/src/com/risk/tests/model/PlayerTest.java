7ed72bc6b356d6752bda78cbb992640ba77e8ec0
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-10-20 14:06:05 -0400

    Creating Test Class for Player Test

---
+++ Risk/src/com/risk/tests/model/PlayerTest.java
@@ -0,0 +1,19 @@
+package com.risk.tests.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * PlayerTest
+ * 
+ * This Class will run tests on model/Player
+ * 
+ * @author Hamid
+ * @version 1.0
+ */
+
+public class PlayerTest{
+
+}
