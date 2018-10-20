1a005d2f63694ddbb4ef3b13835f7f685bfbc4fc
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-10-20 13:36:35 -0400

    adding java doc for the test class

--- Risk/src/com/risk/tests/view/FortifyWindowTest.java
+++ Risk/src/com/risk/tests/view/FortifyWindowTest.java
@@ -5,6 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * FortifyWindowTest
+ * 
+ * This Class will run tests on view/FortifyWindow
+ * 
+ * @author Hamid
+ * @version 1.0
+ */
+
 public class FortifyWindowTest{
 
 }
