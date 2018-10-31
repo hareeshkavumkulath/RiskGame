0c090a09706c7d0464e1ccad2b1dbad90f59080a
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-10-31 00:21:02 -0400

    Conflict remove

--- Risk/src/com/risk/view/AttackView.java
+++ Risk/src/com/risk/view/AttackView.java
@@ -9,9 +9,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import com.risk.model.Player;
-
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import java.awt.Component;
@@ -19,8 +16,6 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 /**
  * @author Hareesh Kavumkulath
@@ -29,26 +24,37 @@ import java.awt.event.ActionEvent;
 public class AttackView extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField attackingArmy;
-	private JTextField attackedArmy;
-	
-	private Player player;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AttackView frame = new AttackView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the frame.
 	 */
-	public AttackView(Player player) {
-		
-		this.player = player;
-		
+	public AttackView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1215, 590);
+		setBounds(100, 100, 1148, 590);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Attacking Territory");
+		JLabel lblNewLabel = new JLabel("Attacking Country");
 		lblNewLabel.setBounds(244, 16, 151, 20);
 		contentPane.add(lblNewLabel);
 		
@@ -56,70 +62,48 @@ public class AttackView extends JFrame {
 		label.setBounds(15, 16, 151, 20);
 		contentPane.add(label);
 		
-		JLabel lblAttackedCountry = new JLabel("Attacked Territory");
+		JLabel lblAttackedCountry = new JLabel("Attacked Country");
 		lblAttackedCountry.setBounds(563, 16, 151, 20);
 		contentPane.add(lblAttackedCountry);
 		
-		JLabel playerName = new JLabel(player.getName());
-		playerName.setBounds(15, 57, 69, 20);
-		contentPane.add(playerName);
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(15, 57, 69, 20);
+		contentPane.add(lblNewLabel_1);
 		
 		JScrollPane scrollPane = new JScrollPane((Component) null);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setBounds(244, 52, 247, 303);
 		contentPane.add(scrollPane);
 		
-		JTextPane attackingTerr = new JTextPane();
-		attackingTerr.setText("");
-		scrollPane.setViewportView(attackingTerr);
+		JTextPane textPane = new JTextPane();
+		textPane.setText("");
+		scrollPane.setViewportView(textPane);
 		
 		JScrollPane scrollPane_1 = new JScrollPane((Component) null);
 		scrollPane_1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane_1.setBounds(563, 52, 247, 303);
 		contentPane.add(scrollPane_1);
 		
-		JTextPane attackedTerr = new JTextPane();
-		attackedTerr.setText("");
-		scrollPane_1.setViewportView(attackedTerr);
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setText("");
+		scrollPane_1.setViewportView(textPane_1);
 		
-		attackingArmy = new JTextField();
-		attackingArmy.setBounds(244, 371, 247, 26);
-		contentPane.add(attackingArmy);
-		attackingArmy.setColumns(10);
+		textField = new JTextField();
+		textField.setBounds(244, 371, 247, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
 		
-		attackedArmy = new JTextField();
-		attackedArmy.setColumns(10);
-		attackedArmy.setBounds(563, 371, 247, 26);
-		contentPane.add(attackedArmy);
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(563, 371, 247, 26);
+		contentPane.add(textField_1);
 		
 		JLabel lblNumberOfDices = new JLabel("Number of Dices");
 		lblNumberOfDices.setBounds(15, 374, 167, 20);
 		contentPane.add(lblNumberOfDices);
 		
-		JButton btnAttack = new JButton("Attack");
-		btnAttack.setBounds(655, 442, 151, 56);
-		contentPane.add(btnAttack);
-		
-		JTextPane statusPanel = new JTextPane();
-		statusPanel.setText("");
-		
-		JScrollPane scrollPane_2 = new JScrollPane(statusPanel);
-		scrollPane_2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane_2.setBounds(879, 52, 299, 345);
-		contentPane.add(scrollPane_2);
-		
-		JButton btnEndAttack = new JButton("End Attack");
-		btnEndAttack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnEndAttack.setBounds(832, 442, 151, 56);
-		contentPane.add(btnEndAttack);
-		
-		JButton btnFortify = new JButton("Fortify");
-		btnFortify.setBounds(1011, 442, 151, 56);
-		contentPane.add(btnFortify);
-		
-		setVisible(true);
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(695, 428, 115, 29);
+		contentPane.add(btnNewButton);
 	}
 }
