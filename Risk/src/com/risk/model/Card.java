7574b7f7d70cd21d8643cc21586623e95f848f09
AnqiAngelineWang <anqi.angeline.wang@gmail.com>
2018-10-31 23:52:22 -0400

    add java doc

--- Risk/src/com/risk/model/Card.java
+++ Risk/src/com/risk/model/Card.java
@@ -11,24 +11,54 @@ package com.risk.model;
  *
  */
 public class Card {
-
+	
+	@SuppressWarnings("javadoc")
 	public String armyType;
+	@SuppressWarnings("javadoc")
 	public int numArmies;
 	
+	/**
+	 * Constructor with parameter for Card class.
+	 * 
+	 * @param armyType the armyType variable will be initialized by constructor
+	 */
 	public Card(String armyType) {
 		super();
 		this.armyType = armyType;
 	}
 	
+	/** 
+	 * Getter method for the armyType of the Card class
+	 * 
+	 * @return String armyType of the card
+	 */
 	public String getArmyType() {
 		return armyType;
 	}
+	
+	/**
+	 * Setter for the armyType of the Card class
+	 * 
+	 * @param armyType set the armyType of the card
+	 */
 	public void setArmyType(String armyType) {
 		this.armyType = armyType;
 	}
+	
+	/** 
+	 * Getter method for the number of armies of the Card class
+	 * 
+	 * @return int numArmies of the card
+	 */
 	public int getNumArmies() {
 		return numArmies;
 	}
+	
+	/**
+	 * Setter for the name of the player
+	 * 
+	 * @param name of the player
+	 */
 	public void setNumArmies(int numArmies) {
 		this.numArmies = numArmies;
 	}
