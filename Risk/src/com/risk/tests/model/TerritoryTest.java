d7b99c9b87768753890aff87543733ba4d0cdab2
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-11-07 13:40:09 -0500

    adding test file/class for Territory Model

---
+++ Risk/src/com/risk/tests/model/TerritoryTest.java
@@ -0,0 +1,21 @@
+package com.risk.tests.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.risk.model.Territory;
+
+/**
+ * TerritoryTest
+ * 
+ * This Class will run tests on model/Territory
+ * 
+ * @author Hamid
+ * @version 1.0
+ */
+
+public class TerritoryTest{
+
+}
