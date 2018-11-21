44ee26243e49d35cbc96c2247aa0dc0c2dee3ce5
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-11-20 21:48:12 -0500

    Cheater player strategies has been added

--- Risk/src/com/risk/model/CheaterPlayer.java
+++ Risk/src/com/risk/model/CheaterPlayer.java
@@ -6,36 +6,53 @@ package com.risk.model;
 import com.risk.controller.GameController;
 
 /**
+ * Strategy class for Cheater Player
+ * 
  * @author Angeline Anqi Wang
  *
  */
 public class CheaterPlayer implements Strategy {
 
-	/* (non-Javadoc)
-	 * @see com.risk.model.Strategy#reinforce(com.risk.model.Player, com.risk.model.GameInstructions, com.risk.controller.GameController)
-	 */
 	@Override
 	public void reinforce(Player currentPlayer, GameInstructions gameInstructions, GameController controller) {
-		// TODO Auto-generated method stub
-
+		int reinforcementArmy = controller.calculateReinforcementArmiesForCheater(currentPlayer);
+		currentPlayer.setNumberOfArmies(reinforcementArmy);
+		gameInstructions.setInstructions(currentPlayer.getName() + " has " + reinforcementArmy + " reinforcement armies.\n");	
+		for(int i=0;i<currentPlayer.getOwnedTerritories().size();i++) {
+			controller.addArmyToTerritory(currentPlayer, currentPlayer.getOwnedTerritories().get(i), currentPlayer.getOwnedTerritories().get(i).getNumberOfArmies());
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see com.risk.model.Strategy#attack(com.risk.model.Player, com.risk.model.GameInstructions, com.risk.controller.GameController, com.risk.model.Game)
-	 */
 	@Override
 	public void attack(Player currentPlayer, GameInstructions gameInstructions, GameController controller, Game game) {
-		// TODO Auto-generated method stub
-
+		for(int i=0;i<currentPlayer.getOwnedTerritories().size();i++) {
+			Territory attackerTerr = currentPlayer.getOwnedTerritories().get(i);
+			for(int j=0;j<attackerTerr.getAdjacentTerritories().size();j++) {
+				Territory opponentTerr = attackerTerr.getAdjacentTerritories().get(j);
+				if(opponentTerr.getRuler() != currentPlayer) {
+					gameInstructions.setInstructions(currentPlayer.getName() + " has conquered " + opponentTerr.getName() +"\n");
+					Player opponentRuler = opponentTerr.getRuler();
+					controller.updateTerritoryRuler(opponentTerr, attackerTerr.getRuler(), 1);
+					controller.updateTerritoryRuler(attackerTerr, attackerTerr.getRuler(), attackerTerr.getNumberOfArmies());
+					controller.updateAddPlayerList(opponentTerr, attackerTerr.getRuler());
+					controller.updateRemovePlayerList(opponentTerr, opponentRuler);
+					controller.updatePlayerList();
+				}
+			}
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see com.risk.model.Strategy#fortify(com.risk.model.Player, com.risk.model.GameInstructions, com.risk.controller.GameController)
-	 */
 	@Override
 	public void fortify(Player currentPlayer, GameInstructions gameInstructions, GameController controller) {
-		// TODO Auto-generated method stub
-
+		for(int i=0;i<currentPlayer.getOwnedTerritories().size();i++) {
+			Territory territory = currentPlayer.getOwnedTerritories().get(i);
+			Territory opponentTerr = controller.getOpponent(currentPlayer, territory);
+			if(opponentTerr != null) {
+				int numArmy = territory.getNumberOfArmies();
+				territory.setNumberOfArmies(numArmy*2);
+				gameInstructions.setInstructions(currentPlayer.getName() + " has fortified " + territory.getName() + " with " + numArmy + " armies\n");
+			}
+		}
 	}
 
 }
