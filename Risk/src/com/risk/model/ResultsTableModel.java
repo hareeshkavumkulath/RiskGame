f24ef1283ced0731360813997191ede57a085ef4
panjingya <798290172@qq.com>
2018-11-25 17:26:23 -0500

    Added javadoc

--- Risk/src/com/risk/model/ResultsTableModel.java
+++ Risk/src/com/risk/model/ResultsTableModel.java
@@ -29,19 +29,38 @@ public class ResultsTableModel extends AbstractTableModel{
 		this.data = data;
 	}
 	
-
+	/**
+	 * Getter to get the table column
+	 * 
+	 * @return int number of column
+	 */
 	public int getColumnCount() {
       return columnNames.length;
     }
 
+	/**
+	 * Getter to get the table row
+	 * 
+	 * @return int number of row
+	 */
     public int getRowCount() {
       return data.length;
     }
 
+    /**
+     * Getter to get the column name
+     * 
+     * @return String column name
+     */
     public String getColumnName(int col) {
       return columnNames[col];
     }
 
+    /**
+     * Getter to get the value in the table
+     * 
+     * @return Object value
+     */
     public Object getValueAt(int row, int col) {
       return data[row][col];
     }
