f55ee6319fb3f2750a5108895a808eaaa12c744e
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-11-26 18:57:32 -0500

    adding SelectGameWindowTest(view) for load functionality test

---
+++ Risk/src/com/risk/tests/view/SelectGameWindowTest.java
@@ -0,0 +1,77 @@
+package com.risk.tests.view;
+
+/**
+ * SelectGameWindowTests
+ * 
+ * This Class will test the  SelectGameWindow functionalities
+ * 
+ * @author Hamid
+ * @version 1.0
+ */
+
+import org.junit.jupiter.api.Test;
+
+import com.risk.model.Game;
+import com.risk.view.SelectGameWindow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.Assert.assertEquals;
+
+
+public class SelectGameWindowTest {
+
+	@SuppressWarnings("javadoc")
+    private  SelectGameWindow selectGameWindow;
+	
+
+    /**
+     * Before Each Test it Creates a new object from Map Controller 
+     */
+    @BeforeEach
+    void init() {
+    	selectGameWindow = new SelectGameWindow();
+    }
+    
+    // =====================loadGameFromFile()=====================
+    /**
+     * Testing loadGameFromFile function return value for Valid Saved File
+     * FOR Test.game file:
+     * Number Of Continents should equals 4
+     * NUmber of Territories should equal 58
+     * Current Player.name should Equal to Player1
+     * 
+     */
+    @Test
+    @DisplayName("loadGameFromFile() => TRUE for Existing GameFile")
+    void loadGameFromFileValid() {
+    	File file = new File(".\\Games\\" + "ForTestClass.game");
+    	Game savedGame = selectGameWindow.loadGameFromFile(file);
+    	
+    	assertEquals(7,savedGame.map.getContinents().size());
+    	assertEquals(58,savedGame.map.getTerritories().size());
+    	assertEquals("Player1",savedGame.currentPlayer.name);
+   
+    }
+    
+    /**
+     * Testing loadGameFromFile function return value for InValid SavedGame
+     * For NotValid.game file should RETURN NULL
+     */
+    @Test
+    @DisplayName("loadGameFromFile() => NULL for Non Existing GameFile")
+    void loadGameFromFileInValid() {
+    	File file = new File(".\\Games\\" + "NotValid.game");
+    	Game savedGame = selectGameWindow.loadGameFromFile(file);
+    	
+    	assertEquals(null,savedGame);
+   
+    }
+ 
+}
