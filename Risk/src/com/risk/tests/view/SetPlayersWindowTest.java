c3db46b9880c06c3a03c1e119ae5739b35abe87c
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-11-28 22:10:41 -0500

    adding Set PlayerWindow test for startup phase test

---
+++ Risk/src/com/risk/tests/view/SetPlayersWindowTest.java
@@ -0,0 +1,152 @@
+package com.risk.tests.view;
+/**
+ * SetPlayersWindowTests
+ * 
+ * This Class will test the  SetPlayersWindow functionalities
+ * (For validation of correct startup phase)
+ * 
+ * @author Hamid
+ * @version 1.0
+ */
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.Assert.assertEquals;
+
+import com.risk.controller.GameController;
+import com.risk.controller.MapController;
+import com.risk.model.AggressivePlayer;
+import com.risk.model.BenevolentPlayer;
+import com.risk.model.Card;
+import com.risk.model.CheaterPlayer;
+import com.risk.model.Game;
+import com.risk.model.HumanPlayer;
+import com.risk.model.MapMessage;
+import com.risk.model.Player;
+import com.risk.model.RandomPlayer;
+import com.risk.view.SetPlayersWindow;
+
+public class SetPlayersWindowTest {
+
+	@SuppressWarnings("javadoc")
+    private  static String mapFolder;
+	@SuppressWarnings("javadoc")
+    private  static String world;
+	@SuppressWarnings("javadoc")
+	private MapMessage worldResult;
+	@SuppressWarnings("javadoc")
+	private SetPlayersWindow setPlayersWindow;
+	
+	/**
+     * Before All sets the local address for Valid and Invalid maps 
+     */
+    @BeforeAll
+    static void initAll() {
+        mapFolder = System.getProperty("user.dir") + "/src/com/risk/tests/controller/maps/";
+        world = mapFolder + "world.map";
+    }
+    /**
+     * Before Each Test it :
+     * Reads a Valid map
+     * Creates an instance of the SetPlayersWindow for testing
+     */
+    @BeforeEach
+    void init() {
+    	//Setting MAP
+    	worldResult = mapToMapMessage(world);
+    	//Creating a setPlayersWindow using the map
+    	setPlayersWindow = new SetPlayersWindow(worldResult.getMap());
+    }
+    
+    // =====================setStartupPhase()=====================
+	 /**
+     * Testing setStartupPhase function return value for Valid Startup Phase
+     * Cards, Players and their Strategies, Map, CurrentPlayer
+     */
+    @Test
+    @DisplayName("setStartupPhase() => GameContoller Should Contain all Elements")
+    void setStartupPhaseValid() {
+    	//Number of armies for 5 players
+    	GameController gc = new GameController();
+		int numArmies = gc.getPlayersArmies(5);
+		
+    	//Setting Players
+    	//5 Players with different strategies
+    	 ArrayList<Player> playerList = new ArrayList<Player>();
+    	 playerList.add(setPlayer("Player1",false,numArmies,"ADD","Human"));
+    	 playerList.add(setPlayer("Player2",true,numArmies,"ADD","AGGRESSIVE"));
+    	 playerList.add(setPlayer("Player3",true,numArmies,"ADD","BENEVOLENT"));
+    	 playerList.add(setPlayer("Player4",true,numArmies,"ADD","RANDOM"));
+    	 playerList.add(setPlayer("Player5",true,numArmies,"ADD","CHEATER"));
+    	
+    	 //Setting Cards
+    	 ArrayList<Card> cards = gc.loadCards(worldResult.map.getTerritories().size());
+    	 
+    	//Passing all the Game elements to the function
+    	 GameController gameController = setPlayersWindow.setStartupPhase(worldResult.map,cards,playerList);
+    	 
+    	//testing the result
+    	
+    }
+// =====================Helper Functions()=====================
+// =====================mapToString()=====================
+    
+    /**
+     * 
+     * Function for reading a map file and convert that into String
+     * 
+     * @param mapFile Map File Address as a String
+     * @return String Map Content to Strings
+     */
+    private static String mapToString(String mapFile) {
+        String mapInString = "";
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(mapFile));
+
+            String line = reader.readLine();
+            mapInString += line;
+
+            while (line != null) {
+                mapInString += line;
+                line = reader.readLine();
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            mapInString = null;
+        }
+        return mapInString;
+    }
+    // 	=====================mapToMapMessage()=====================
+    /**
+     * 
+     * Function for processing a map and returning 
+     * the result as a MapMessage
+     * 
+     * @param String mapName for the Name of the map 
+     * @return MapMessage mapMessage which is the processed result 
+     */
+
+    private static MapMessage mapToMapMessage(String mapName) {
+    	
+    		MapController mpc = new MapController();
+    		mpc.processContinents(mapToString(mapName));
+    		mpc.processTerritories(mapToString(mapName));
+    		mpc.territoriesToContinents();
+        MapMessage mapMessage = mpc.processFile(new File(mapName));
+        return mapMessage;
+        
+    }
+    
+}
